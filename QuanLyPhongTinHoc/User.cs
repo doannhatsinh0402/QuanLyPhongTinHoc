0bc128d469b82113b29b55814f14dba0790fccb4
doannhatsinh0402 <19133049@student.hcmute.edu.vn>
2020-12-21 19:24:21 +0700

    Tham 21-12-2020

--- QuanLyPhongTinHoc/User.cs
+++ QuanLyPhongTinHoc/User.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace QuanLyPhongTinHoc
 {
-    public class User : Human
+    public class User:Human
     {
+        public string checkIn(params object[]thamso)
+        {
+            return "ngay su dung + ten thiet bi hu hong";
+        }
+        public string compensationDV(params object[]thamso)
+        {
+            
+            return "den bu theo quy dinh";
+        }
     }
 }
